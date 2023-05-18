47cb7324af3c72ecda4dd7a0947e748c3b283659
khachatryantigran <tigran9202@gmail.com>
2023-05-18 18:08:21 +0400

    change env.local

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "http://api.weatherapi.com/v1/", // process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
   config.url =
-    config.url + "&units=metric" + "&appid=" + process.env.REACT_APP_API_KEY;
+    config.url +
+    "&units=metric" +
+    "&appid=" +
+    "45fd324fcb36497bae5111643232804"; //process.env.REACT_APP_API_KEY;
   return config;
 });
 
